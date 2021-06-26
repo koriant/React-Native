f6b4bd3666f5c644a3a9e2e3a601f0ce5660e3fa
chris <ckim0707@gmail.com>
2021-06-25 19:19:48 -0500

    React Native Week 2 Forms

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -5,6 +5,7 @@ import About from './AboutComponent';
 import Contact from './ContactComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import Constants from 'expo-constants';
+import Reservation from './ReservationComponent';
 import { View, Platform, StyleSheet, Text, ScrollView, Image } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer';
@@ -120,6 +121,29 @@ const ContactNavigator = createStackNavigator(
     }
 );
 
+const ReservationNavigator = createStackNavigator(
+    {
+        Reservation: { screen: Reservation },
+    }, 
+    {
+        defaultNavigationOptions: ({navigation}) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                name='tree'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
+    }
+);
+
 const CustomDrawerContentComponent = props => (
     <ScrollView>
         <SafeAreaView
@@ -168,7 +192,22 @@ const MainNavigator = createDrawerNavigator(
                         color={tintColor}
                     />
                 )
-            } },
+            } 
+        },
+        Reservation: { 
+            screen: ReservationNavigator,
+            navigationOptions: {
+                drawerLabel: 'Reserve Campsite',
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='tree'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            } 
+        },
         About: { 
             screen: AboutNavigator,
             navigationOptions: {
@@ -181,7 +220,8 @@ const MainNavigator = createDrawerNavigator(
                         color={tintColor}
                     />
                 )
-            }},
+            }
+        },
         Contact: { 
             screen: ContactNavigator,
             navigationOptions: {
@@ -194,7 +234,8 @@ const MainNavigator = createDrawerNavigator(
                         color={tintColor}
                     />
                 )
-            }}
+            }
+        },
     },
     {
         drawerBackgroundColor: '#CEC8FF',
