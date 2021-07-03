f1ff30fd41bc1034e9fe9a22bec078319f81bd53
chris <ckim0707@gmail.com>
2021-07-03 11:39:15 -0500

    React Native Week 4 Workshop

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -97,9 +97,9 @@ class Reservation extends Component {
                             onPress={() => 
                                 Alert.alert(
                                     'Begin Search?',
-                                    `Number of Campers: ${this.state.campers}\
-                                    Hike-In?: ${this.state.hikeIn}\
-                                    Date: ${this.state.date.toLocaleDateString('en-US')}`,
+                                    `Number of Campers: ${this.state.campers}
+                                    \nHike-In?: ${this.state.hikeIn}
+                                    \nDate: ${this.state.date.toLocaleDateString('en-US')}`,
                                     [
                                         {
                                             text: 'Cancel',
