58a34e9fdc08e5ec3b5849ea3263ebee7d269de8
chris <ckim0707@gmail.com>
2021-07-10 12:14:39 -0500

    React Native Week 4 Assignment

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -323,22 +323,24 @@ const AppNavigator = createAppContainer(MainNavigator);
 
 class Main extends Component {
 
+    showNetInfo = async() => { 
+        console.log("hello")
+        let connectionInfo = await NetInfo.fetch();
+        (Platform.OS === 'ios')
+            ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
+            : ToastAndroid.show('Initial Network Connectivity Type: ' + connectionInfo.type, ToastAndroid.LONG);
+    }
+
     componentDidMount() {
         this.props.fetchCampsites();
         this.props.fetchComments();
         this.props.fetchPromotions();
         this.props.fetchPartners();
+        this.showNetInfo();
 
-
-        NetInfo.fetch().then(connectionInfo => {
-            (Platform.OS === 'ios')
-                ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
-                : ToastAndroid.show('Initial Network Connectivity Type: ' + connectionInfo.type, ToastAndroid.LONG);
-        });
-
-        this.unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
-            this.handleConnectivityChange(connectionInfo);
-        });
+        // // this.unsubscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
+        // //     this.handleConnectivityChange(connectionInfo);
+        // });
     }
 
     componentWillUnmount() {
